724eb2fcce666becd935077beb01208e1a2ad95d
Gilson Dias <gilsondiasjn@gmail.com>
2025-08-18 11:39:28 -0300

    Atividade comparação das idades dos alunos

--- StartC/Program.cs
+++ StartC/Program.cs
@@ -6,7 +6,33 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String aluno1, aluno2;
+            int age1, age2;
+
+            Console.WriteLine("Insira o seu nome do primeiro aluno: ");
+            aluno1 = Console.ReadLine();
+
+            Console.WriteLine("Insira a idade desse aluno:");
+            age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o seu nome do segundo aluno: ");
+            aluno2 = Console.ReadLine();
+
+            Console.WriteLine("Insira a idade desse aluno:");
+            age2 = int.Parse(Console.ReadLine());
+
+            if (age1 > age2)
+            {
+                Console.WriteLine($"{aluno1} é o mais velho e o {aluno2} é o mais novo.");
+            }
+            if (age2 > age1)
+            {
+                Console.WriteLine($"{aluno2} é o mais velho e o {aluno1} é o mais novo.");
+            }
+            if (age1 == age2)
+            {
+                Console.WriteLine("Ambos os alunos têm a mesma idade.");
+            }
         }
     }
 }
