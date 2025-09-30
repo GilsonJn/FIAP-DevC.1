67cde0300e438239809698d64b16e84966077f5d
Gilson Dias <gilsondiasjn@gmail.com>
2025-09-30 18:08:17 -0300

    Atividade de Classes

---
+++ DevC/Classes/Carro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Carro
+    {
+        private string chassis;
+        private string licensePlate;
+        private string cor;
+        private string motor;
+
+        public Carro(string chassis, string licensePlate, string cor, string motor)
+        {
+            this.chassis = chassis;
+            this.licensePlate = licensePlate;
+            this.cor = cor;
+            this.motor = motor;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Detalhes do carro: \n" +
+                $"Chassis: {chassis} Placa: {licensePlate} Cor: {cor} Motor: {motor}\n";
+        }
+    }
+}
---
+++ DevC/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using Classes;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro[] carList = new Carro[]
+            {
+                new Carro("Sedan", "ABC1234", "Vermelho", "V6"),
+                new Carro("SUV", "XYZ7890", "Azul", "Elétrico"),
+                new Carro("Hatchback", "HCK4567", "Preto", "1.4 Turbo"),
+                new Carro("Coupé", "CPE1122", "Branco", "V8"),
+                new Carro("Perua", "PRU3344", "Prata", "2.0 Diesel"),
+                new Carro("Conversível", "CON5566", "Amarelo", "V6"),
+                new Carro("Picape", "PIC7788", "Verde", "3.0 Diesel"),
+                new Carro("Minivan", "MIN9900", "Azul Claro", "Híbrido"),
+                new Carro("Esportivo", "ESP1213", "Laranja", "V10"),
+                new Carro("Compacto", "CMP1415", "Roxo", "1.0")
+            };
+
+            foreach (Carro car in carList)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
