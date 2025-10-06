c0c645f40119afa7353fe9cc6d6b731c3519ccb0
Gilson Dias <gilsondiasjn@gmail.com>
2025-10-05 23:41:52 -0300

    atividade livraria

---
+++ DevC/AtividadeLivraria/Livro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeLivraria
+{
+    internal class Livro
+    {
+        protected string titulo;
+        protected string autor;
+        protected int ano;
+        protected string publicacao;
+        protected bool status = true;
+
+        public Livro(string titulo, string autor, int ano, string publicacao)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.ano = ano;
+            this.publicacao = publicacao;
+        }
+
+        public void Detalhes()
+        {
+            Console.WriteLine("Título: " + titulo);
+            Console.WriteLine("Autor: " + autor);
+            Console.WriteLine("Ano: " + ano);
+            Console.WriteLine("Publicação: " + publicacao);
+        }
+
+        public Boolean Emprestado()
+        {
+            return status = false;
+        }
+
+        public Boolean Devolvido()
+        {
+            return status = true;
+        }
+
+        public string Status()
+        {
+            if (status)
+            {
+                return "Status: Disponível";
+            }
+            else
+            {
+                return "Status: Emprestado";
+            }
+        }
+    }
+}
---
+++ DevC/AtividadeLivraria/Program.cs
@@ -0,0 +1,22 @@
+﻿using AtividadeLivraria;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro("AAA", "Jonas", 2002, "10/10/2002");
+            Livro livro2 = new Livro("BBB", "Maria", 2010, "15/05/2010");
+
+            livro1.Detalhes();
+            Console.WriteLine(livro1.Status());
+
+            livro2.Detalhes();
+            livro2.Emprestado();
+            Console.WriteLine(livro2.Status());
+
+        }
+    }
+}
